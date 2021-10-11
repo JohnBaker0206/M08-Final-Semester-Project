8391e414e66beae5182b5202b1da045441b83dff
JohnBaker0206 <92338223+JohnBaker0206@users.noreply.github.com>
2021-10-11 17:15:50 -0400

    Delete InvestmentProject directory

--- InvestmentProject/src/Enums/RealEstateType.java
+++
@@ -1,6 +0,0 @@
-package Enums;
-
-// My enum for the type of the Read estate
-public enum RealEstateType {
-    RESIDENTIAL, COMMERCIAL
-}
--- InvestmentProject/src/Enums/StockType.java
+++
@@ -1,6 +0,0 @@
-package Enums;
-
-//My enum for the stock type
-public enum StockType {
-    NASDAQ, NYSE
-}
